7f3843d2277a135b343c1ba2fcd02a9f6470691a
g.bremont-ext <g.bremont-ext@betclicgroup.com>
2025-10-15 13:46:40 +0200

    refacto: move sellIn condition inside

--- gilded-rose/TypeScript/app/gilded-rose.ts
+++ gilded-rose/TypeScript/app/gilded-rose.ts
@@ -53,14 +53,20 @@ export class GildedRose {
       item.sellIn = item.sellIn - 1;
 
 
-      if (item.sellIn < 0) {
-        if (item.name == AGED_BRIE) {
-          if (item.quality < 50) {
+      if (item.name == AGED_BRIE) {
+        if (item.quality < 50) {
+          if (item.sellIn < 0) {
             item.quality = item.quality + 1
           }
-        } else if (item.name == BACKSTAGE_PASS) {
+        }
+      } else if (item.name == BACKSTAGE_PASS) {
+        if (item.sellIn < 0) {
+
           item.quality = item.quality - item.quality
-        } else {
+        }
+      } else {
+        if (item.sellIn < 0) {
+
           if (item.quality > 0) {
             item.quality = item.quality - 1
           }
