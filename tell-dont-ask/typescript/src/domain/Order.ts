25ce61a71e9990721057fba2158cc7d6177ffdf2
g.bremont-ext <g.bremont-ext@betclicgroup.com>
2025-11-14 15:14:18 +0100

    refacto: move logic for order approval inside the Order class

--- tell-dont-ask/typescript/src/domain/Order.ts
+++ tell-dont-ask/typescript/src/domain/Order.ts
@@ -1,61 +1,92 @@
 import OrderItem from './OrderItem';
-import { OrderStatus } from './OrderStatus';
+import {OrderStatus} from './OrderStatus';
+import RejectedOrderCannotBeApprovedException from "../useCase/exception/RejectedOrderCannotBeApprovedException";
+import ApprovedOrderCannotBeRejectedException from "../useCase/exception/ApprovedOrderCannotBeRejectedException";
+import ShippedOrdersCannotBeChangedException from "../useCase/exception/ShippedOrdersCannotBeChangedException";
 
 class Order {
-  private total: number;
-  private currency: string;
-  private items: OrderItem[];
-  private tax: number;
-  private status: OrderStatus;
-  private id: number;
+    private total: number;
+    private currency: string;
+    private items: OrderItem[];
+    private tax: number;
+    private status: OrderStatus;
+    private id: number;
 
-  public getTotal(): number {
-      return this.total;
-  }
+    public getTotal(): number {
+        return this.total;
+    }
 
-  public setTotal(total: number): void  {
-      this.total = total;
-  }
+    public setTotal(total: number): void {
+        this.total = total;
+    }
 
-  public getCurrency(): string {
-      return this.currency;
-  }
+    public getCurrency(): string {
+        return this.currency;
+    }
 
-  public setCurrency(currency: string): void {
-      this.currency = currency;
-  }
+    public setCurrency(currency: string): void {
+        this.currency = currency;
+    }
 
-  public getItems(): OrderItem[] {
-      return this.items;
-  }
+    public getItems(): OrderItem[] {
+        return this.items;
+    }
 
-  public setItems(items: OrderItem[]): void {
-      this.items = items;
-  }
+    public setItems(items: OrderItem[]): void {
+        this.items = items;
+    }
 
-  public getTax(): number {
-      return this.tax;
-  }
+    public getTax(): number {
+        return this.tax;
+    }
 
-  public setTax(tax: number): void {
-      this.tax = tax;
-  }
+    public setTax(tax: number): void {
+        this.tax = tax;
+    }
 
-  public getStatus(): OrderStatus {
-      return this.status;
-  }
+    public getStatus(): OrderStatus {
+        return this.status;
+    }
 
-  public setStatus(status: OrderStatus): void {
-      this.status = status;
-  }
+    public setStatus(status: OrderStatus): void {
+        this.status = status;
+    }
 
-  public getId(): number {
-      return this.id;
-  }
+    public getId(): number {
+        return this.id;
+    }
 
-  public setId(id: number): void {
-      this.id = id;
-  }
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public hasBeenShipped(): boolean {
+        return this.status === OrderStatus.SHIPPED
+    }
+
+    public isRejected(): boolean {
+        return this.status === OrderStatus.REJECTED
+    }
+
+    public isApproved() {
+        return this.status === OrderStatus.APPROVED
+    }
+
+    public approve() {
+        if (this.hasBeenShipped()) throw new ShippedOrdersCannotBeChangedException();
+
+        if (this.isRejected()) throw new RejectedOrderCannotBeApprovedException();
+
+        this.status = OrderStatus.APPROVED;
+    }
+
+    public reject() {
+        if (this.hasBeenShipped()) throw new ShippedOrdersCannotBeChangedException();
+
+        if (this.isApproved()) throw new ApprovedOrderCannotBeRejectedException();
+
+        this.status = OrderStatus.REJECTED;
+    }
 }
 
 export default Order;
